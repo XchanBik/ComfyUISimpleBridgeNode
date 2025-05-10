62c11092191ce669d2ecf568f88a5f56f93812e3
XchanBik <xchanbik@gmail.com>
2025-05-10 16:59:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,26 +3,24 @@
 @nickname: SimpleBridgeNode
 @description: A custom node for ComfyUI to store and retrieve data dynamically.
 """
-from .wai_illustrious_character_select import llm_prompt_gen_node, illustrious_character_select, illustrious_character_select_en, local_llm_prompt_gen
-from .image_saver.image_saver import ImageSaver
+from .Text import SimpleTextLoader
+from .Bridge import SimpleBridgeStoreNode, SimpleBridgeLoadNode
 
 def __init__(self):
     pass
     
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
+# Node mapping
 NODE_CLASS_MAPPINGS = {
-    "IntMultiplication"         : IntMultiplication,
-
-    "ImageSaverMira"                 : ImageSaver,
+    "SimpleBridgeStoreNode": SimpleBridgeStoreNode,
+    "SimpleBridgeLoadNode": SimpleBridgeLoadNode,
+    "SimpleTextLoader": TextLoader
 }
 
-# A dictionary that contains the friendly/humanly readable titles for the nodes
+# Node display names
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "IntMultiplication"         : "Integer Multiplication",
+    "SimpleBridgeStoreNode": "SimpleBridge Store",
+    "SimpleBridgeLoadNode": "SimpleBridge Load",
+    "SimpleTextLoader": "FileText as String"
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
-
-from .xchanbik_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']

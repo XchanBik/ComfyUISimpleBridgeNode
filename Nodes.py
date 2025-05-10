79abc9cd39a0908c14b359647fd030f6242c67d0
XchanBik <xchanbik@gmail.com>
2025-05-10 16:56:27 +0200

    Update and rename xchanbik_nodes.py to Nodes.py

---
+++ Nodes.py
@@ -0,0 +1,14 @@
+
+# Node mapping
+NODE_CLASS_MAPPINGS = {
+    "SimpleBridgeStoreNode": SimpleBridgeStoreNode,
+    "SimpleBridgeLoadNode": SimpleBridgeLoadNode,
+    "SimpleBridgeDynamic": SimpleBridgeDynamic
+}
+
+# Node display names
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SimpleBridgeStoreNode": "SimpleBridge Store",
+    "SimpleBridgeLoadNode": "SimpleBridge Load",
+    "SimpleBridgeDynamic": "SimpleBridge Dynamic"
+}

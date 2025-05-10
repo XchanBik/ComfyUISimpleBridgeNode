5c8baf1af06fd268f99e819f5ef2274ffb63c53d
XchanBik <xchanbik@gmail.com>
2025-05-10 16:51:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,5 @@
 @description: A custom node for ComfyUI to store and retrieve data dynamically.
 """
 
-WEB_DIRECTORY = "./js"
-
-from .bridge_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .xchanbik_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']

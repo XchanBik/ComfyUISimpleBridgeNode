3c3d40eda01bd9d2e1fb3b814b2386a8412b8111
XchanBik <xchanbik@gmail.com>
2025-05-10 12:21:48 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+@title: SimpleBridgeNode
+@nickname: SimpleBridgeNode
+@description: A custom node for ComfyUI to store and retrieve data dynamically.
+"""
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
